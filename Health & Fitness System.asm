bc6f731bff82cc4f24582499812b254fe9a740ab
Treacherous Taz <118551548+itztazma8@users.noreply.github.com>
2025-09-12 23:45:43 +0600

    Inputs (v1.2)

--- Health & Fitness System.asm
+++ Health & Fitness System.asm
@@ -1,6 +1,6 @@
 .MODEL SMALL    
 
-
+;-----------------------------------------------------------MACRO-1: LINE SKIP----------------------------------------------------------------
 
 SKIP MACRO
     MOV AH, 2
@@ -19,7 +19,9 @@ MES DW "WELCOME TO YOUR WEEKLY FITNESS REPORT SYSTEM$"
 WE DW "Weight: $"
 HE DW "Height: $"
 ST_W DW "Steps Walked: $"
-CAL DW "Daily Calorie Intake: $"
+CAL DW "Daily Calorie Intake: $" 
+FT DW " Feet $"
+INCH DW " Inch$"
 
 .CODE
 MAIN PROC
@@ -27,6 +29,8 @@ MAIN PROC
 MOV AX,@DATA
 MOV DS,AX    
 
+
+;--------------------------------------------------------------------UI-----------------------------------------------------------------------
 MOV CX, 45       
 l1:        
 MOV AH, 2
@@ -53,25 +57,63 @@ MOV DL, "-"
 INT 21H
 LOOP l2 
 
-SKIP
+SKIP  
+
+;----------------------------------------------------------WEIGHT CALCULATION-----------------------------------------------------------------
 
 LEA DX, WE
 MOV AH, 9
 INT 21H
 
+
+limit:
+MOV AH, 1
+INT 21H
+MOV AH, 0
+CMP AL, 13
+JE end
+
+JMP limit
+
+end: 
+
+SKIP   
 SKIP
+
+;-------------------------------------------------------HEIGHT CALCULATION--------------------------------------------------------------------
 
 LEA DX, HE
 MOV AH, 9
 INT 21H
 
-SKIP
+MOV AH, 1
+INT 21H
 
+LEA DX, FT
+MOV AH, 9
+INT 21H
+
+MOV AH, 1
+INT 21H
+MOV AH, 1
+INT 21H
+
+LEA DX, INCH
+MOV AH, 9
+INT 21H
+
+;---------------------------------------------------------STEPS CALCULATION------------------------------------------------------------------
+
+SKIP
+SKIP
 LEA DX, ST_W
 MOV AH, 9
 INT 21H
 
 SKIP
+SKIP
+
+;------------------------------------------------------CALORIE CALCULATION--------------------------------------------------------------------
 
 LEA DX, CAL
 MOV AH, 9
