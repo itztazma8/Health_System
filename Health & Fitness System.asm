824820231303a39eb9e528503f76531b1aa3f59d
Treacherous Taz <118551548+itztazma8@users.noreply.github.com>
2025-09-13 17:19:36 +0600

    Inputs (v1.3)

--- Health & Fitness System.asm
+++ Health & Fitness System.asm
@@ -15,19 +15,30 @@ ENDM
 
 .DATA
 
-MES DW "WELCOME TO YOUR WEEKLY FITNESS REPORT SYSTEM$"    
+MES DW "WELCOME TO YOUR WEEKLY FITNESS REPORT SYSTEM$"  
+NO DW "How many entries you want to make? $"  
 WE DW "Weight: $"
 HE DW "Height: $"
 ST_W DW "Steps Walked: $"
 CAL DW "Daily Calorie Intake: $" 
 FT DW " Feet $"
-INCH DW " Inch$"
+INCH DW " Inch$"   
+RE DW "-------------------$"  
+RE1 DW " REPORT $"
+
+
+WEIGHT DW 7 DUP(0)
+HEIGHT DW 7 DUP(0)
+STEP DW 7 DUP(0)
+CALORIE DW 7 DUP(0)
+
 
 .CODE
 MAIN PROC
 
 MOV AX,@DATA
 MOV DS,AX    
+
 
 
 ;--------------------------------------------------------------------UI-----------------------------------------------------------------------
@@ -50,37 +61,20 @@ SKIP
 SKIP
 SKIP
 
-MOV CX, 45       
+MOV CX, 46       
 l2:        
 MOV AH, 2
 MOV DL, "-"
 INT 21H
 LOOP l2 
 
+SKIP 
 SKIP  
 
-;----------------------------------------------------------WEIGHT CALCULATION-----------------------------------------------------------------
-
-LEA DX, WE
-MOV AH, 9
-INT 21H
 
 
-limit:
-MOV AH, 1
-INT 21H
-MOV AH, 0
-CMP AL, 13
-JE end
+;----------------------------------------------------------HEIGHT CALCULATION-----------------------------------------------------------------
 
-JMP limit
-
-end: 
-
-SKIP   
-SKIP
-
-;-------------------------------------------------------HEIGHT CALCULATION--------------------------------------------------------------------
 
 LEA DX, HE
 MOV AH, 9
@@ -100,7 +94,64 @@ INT 21H
 
 LEA DX, INCH
 MOV AH, 9
+INT 21H  
+
+SKIP
+SKIP     
+
+
+LEA DX, NO 
+MOV AH, 9
 INT 21H
+SKIP
+
+MOV AH, 1
+INT 21H
+
+SUB AL, '0'
+MOV CL, AL
+
+MOV CH, 0
+
+SKIP
+SKIP
+
+MAINLOOP:
+
+;-------------------------------------------------------WEIGHT CALCULATION--------------------------------------------------------------------
+ 
+ 
+LEA DX, RE
+MOV AH, 9
+INT 21H
+
+LEA DX, RE1
+MOV AH, 9
+INT 21H    
+
+LEA DX, RE
+MOV AH, 9
+INT 21H
+
+SKIP
+SKIP
+
+LEA DX, WE
+MOV AH, 9
+INT 21H
+
+
+limit:
+MOV AH, 1
+INT 21H
+MOV AH, 0
+CMP AL, 13
+JE end
+
+JMP limit
+
+end: 
+
 
 ;---------------------------------------------------------STEPS CALCULATION------------------------------------------------------------------
 
@@ -119,8 +170,11 @@ LEA DX, CAL
 MOV AH, 9
 INT 21H 
 
+SKIP 
 SKIP
-               
+
+LOOP MAINLOOP
+             
 MOV AX,4C00H
 INT 21H
 
